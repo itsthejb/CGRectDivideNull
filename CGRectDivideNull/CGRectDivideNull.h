eb9d4eee000df62c76e85efb11614273a81d363c
Jonathan Crooke <joncrooke@gmail.com>
2014-01-14 10:53:33 +0100

    Initial Commit

---
+++ CGRectDivideNull/CGRectDivideNull.h
@@ -0,0 +1,13 @@
+//
+//  CGRectDivideNull.h
+//  CGRectDivideNull
+//
+//  Created by Jonathan Crooke on 14/01/2014.
+//  Copyright (c) 2014 Jonathan Crooke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CGRectDivideNull : NSObject
+
+@end
---
+++ CGRectDivideNull/CGRectDivideNull.m
@@ -0,0 +1,13 @@
+//
+//  CGRectDivideNull.m
+//  CGRectDivideNull
+//
+//  Created by Jonathan Crooke on 14/01/2014.
+//  Copyright (c) 2014 Jonathan Crooke. All rights reserved.
+//
+
+#import "CGRectDivideNull.h"
+
+@implementation CGRectDivideNull
+
+@end
